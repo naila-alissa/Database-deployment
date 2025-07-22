ce3a9ab102af234984e2dda4c4650c93975492e7
naila-alissa <naila.alissa1@inter.ikea.com>
2025-07-22 11:14:17 +0000

    chore: auto-format SQL files with Liquibase headers

--- script/data/insert_test.sql
+++ script/data/insert_test.sql
@@ -1,2 +1,7 @@
+--liquibase formatted sql
+--changeset runneradmin:1-data-insert_test context:data-1 labels:insert_test
+--comment: Change generated for insert_test.sql in data
+
 
 INSERT INTO test_table (id, name, email) VALUES (13, 'Susane', 'ali@example.com');
+
