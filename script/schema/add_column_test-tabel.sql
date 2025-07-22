da85b3b62055ab84f3db7cf81a72b1330a82d60f
naila-alissa <naila.alissa1@inter.ikea.com>
2025-07-22 12:12:45 +0000

    chore: auto-format SQL files with Liquibase headers

--- script/schema/add_column_test-tabel.sql
+++ script/schema/add_column_test-tabel.sql
@@ -1,2 +1,7 @@
+--liquibase formatted sql
+--changeset naila-alissa:1-schema-add_column_test-tabel context:schema-1 labels:add_column_test-tabel
+--comment: Change generated for add_column_test-tabel.sql in schema
+
 
 ALTER TABLE test_table ADD City VARCHAR(250);
+
