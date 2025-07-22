2e978c8dab068c86613f7e2b2f40301278142de8
naila-alissa <naila.alissa1@inter.ikea.com>
2025-07-22 12:35:27 +0000

    chore: auto-format SQL files with Liquibase headers

--- script/schema/add_column_into_table.sql
+++ script/schema/add_column_into_table.sql
@@ -1,1 +1,6 @@
+--liquibase formatted sql
+--changeset naila-alissa:1-schema-add_column_into_table context:schema-1 labels:add_column_into_table
+--comment: Change generated for add_column_into_table.sql in schema
 ALTER TABLE test ADD country VARCHAR(250);
+--rollback: ROLLBACK COMMAND FOR add_column_into_table
+
