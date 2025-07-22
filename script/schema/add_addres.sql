6f42e5df2003787ed1001f16070bdf41968170c2
naila-alissa <naila.alissa1@inter.ikea.com>
2025-07-22 12:40:57 +0000

    chore: auto-format SQL files with Liquibase headers

--- script/schema/add_addres.sql
+++ script/schema/add_addres.sql
@@ -1,2 +1,7 @@
+--liquibase formatted sql
+--changeset naila-alissa:1-schema-add_addres context:schema-1 labels:add_addres
+--comment: Change generated for add_addres.sql in schema
 
 ALTER TABLE test ADD Adresss VARCHAR(250);
+--rollback: ROLLBACK COMMAND FOR add_addres
+
