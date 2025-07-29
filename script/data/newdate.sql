3735c40f408ea66c6c0ea91d08b040f3ebe94638
naila-alissa <naila.alissa1@inter.ikea.com>
2025-07-29 09:13:06 +0000

    chore: auto-format SQL files with Liquibase headers

--- script/data/newdate.sql
+++ script/data/newdate.sql
@@ -1,1 +1,6 @@
+--liquibase formatted sql
+--changeset naila-alissa:data-newdate context:data labels:newdate
+
 INSERT INTO test (id, name, email) VALUES (16, 'Nina', 'ali@example.com');
+--rollback: ROLLBACK newdate
+
