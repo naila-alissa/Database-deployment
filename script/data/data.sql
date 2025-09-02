d0d432ec68491b3e961bf5c7d7344e51e3917092
naila-alissa <naila.alissa1@inter.ikea.com>
2025-09-02 09:50:04 +0000

    chore: auto-format SQL files with Liquibase headers

--- script/data/data.sql
+++ script/data/data.sql
@@ -1,2 +1,7 @@
+--liquibase formatted sql
+--changeset naila-alissa:data-data context:data labels:data
+
 INSERT INTO person_test (id, name, address1, address2, city)
 VALUES (5, 'John Doe', '123 Main St', 'Apt 4B', 'New York');
+
+
